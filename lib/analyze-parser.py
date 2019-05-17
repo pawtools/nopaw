aa48f57fea79ce574f63e8187d42c1c3fb873b3f
jrossyra <jrossyra@gmail.com>
2019-05-17 15:04:14 -0400

    [analyze] constructs started

--- lib/analyze-parser.py
+++ lib/analyze-parser.py
@@ -2,68 +2,100 @@
 
 import os
 import sys
-
+import argparse
 from glob import glob
 from pprint import pformat
-
 import numpy as np
 import anylz
 
-#if __name__ == "__main__":
 #  TODO this is messy wihtout using referenced stamps
-#  FIXME on all the retyped strings and come up with scheme for stamps
+#       --> add to config file
 
-WorkflowStarts = "a worklow starts"
-WorkflowStops  = "a worklow stops"
-WrapperTaskStarts = "starts"
-WrapperTaskStops  = "stops"
-InnerTaskStarts = "starting"
-InnerTaskStops  = "stopping"
+_is_globbable = lambda x: x.find('*') >= 0
 
-if len(sys.argv) > 1:
-    session_directories = glob(os.path.join(sys.argv[1], '*'))
-else:
-    session_directories = glob(os.path.join('weakscaling-rs42', '*'))
+_data_format_template = (
+    "{0: <7} {1: <12} {2: <12} {3: <12} "
+    "{4: <12} {5: <12} {6: <12} {7: <12} "
+    "{8: <12} {9: <12} {10: <12} {11: <12}"
+)
 
-print("{0: <7} {1: <12} {2: <12} {3: <12} {4: <12} {5: <12} {6: <12} {7: <12} {8: <12} {9: <12} {10: <12} {11: <12}".format("N Tasks", "W Dur", "T Dur,avg", "T Dur,std", "T Ini,avg", "T Ini,std", "T I,D,avg", "T I,D,std", "T I,I,avg", "T I,I,std", "T Clz,avg", "T Clz,std"))
+parser = argparse.ArgumentParser(description="Analyze nopaw runs")
+parser.add_argument("session_directories",
+    help="Glob pattern or single directory to analyze"
+)
+parser.add_argument("output_file",
+    nargs="?",
+    help="Full path of output file for writing analysis data"
+)
+parser.add_argument("-c", "--config",
+    default="cfg/analyze.yml",
+    help="Path to a configuration file"
+)
+parser.add_argument("-v", "--verbose",
+    help="Output verbosity level"
+)
 
-workflow_durations = list()
-tasks_durations = list()
-for session_directory in session_directories:
-    if True:
-    #if session_directory.endswith('weak-w-10'):
-        nreplicates = int(session_directory.split('-')[-1])
-    #print(session_directory)
-    #try:
-        #configfile = sys.argv[2]
-     #   deep_session_directory = os.path.join(session_directory, 'sessions')
-     #   deeper_session_directory = os.path.join(
-     #       deep_session_directory,
-     #       list(filter(lambda d: d.startswith('admd-0001.'),
-     #                   os.listdir(deep_session_directory)))[0]
-     #   )
-     #   worker_logfile = os.path.join(deeper_session_directory, "adaptivemd.worker.*")
-     #   workflow_filename = '-'.join(['pG', ''.join(session_directory.split('-'))])
-     #   tasks_filename = glob(worker_logfile)
-        workflow_filename = 'nopaw.out'
-        assert os.path.exists(session_directory)
+
+if __name__ == "__main__":
+
+    # First, handle arguments
+    args = parser.parse_args()
+
+    if not _is_globbable(args.session_directories):
+        # Looking to process MANY   directories
+        session_directories = glob(args.session_directories)
+
+    elif os.path.exists(args.session_directories):
+        # Looking to process SINGLE directories
+        session_directories = [args.session_directories]
+
+    else:
+        print("session_directories must be single folder or set of folders")
+        print("(via glob pattern) containing data for analysis.")
+        print("The given value '%s' does not meet the criteria" % 
+            args.session_directories)
+        print("Exiting")
+        sys.exit(1)
+
+    analyze_configuration = anylz.load_workflow_config(args.config)
+
+    workflow_filename = analyze_configuration['workflow_filename']
+    tasks_folder = analyze_configuration['tasks_folder']
+    tasks_filenames = analyze_configuration['tasks_filenames']
+    n_files_per_task = len(tasks_filenames)
+
+    workload_timestamp_sequence = analyze_configuration['workload_sequence']
+    task_timestamp_sequence = analyze_configuration['task_sequence']
+
+    assert all([_is_globbable(tfnm) for tfnm in tasks_filenames])
+
+    # Second, initialize data structures
+    workflow_durations = list()
+    tasks_durations = list()
+
+    # Third, process the data
+    for session_directory in session_directories:
+
         assert os.path.exists(os.path.join(session_directory, workflow_filename))
-        wrappers_filename = sorted(glob(os.path.join(session_directory, 'executors/nopaw.connector.*')))
-        executors_filename = sorted(glob(os.path.join(session_directory, 'executors/nopaw.executor.*')))
-        tasks_filename = [single for pair in zip(wrappers_filename, executors_filename) for single in pair]
-        workflow_timestamps = {
-            "workflow": {WorkflowStarts, WorkflowStops},
-            "tasks"    : {WrapperTaskStarts, WrapperTaskStops, InnerTaskStarts, InnerTaskStops},
-        }
-        #print(wrappers_filename)
-        #print(executors_filename)
-        #print(workflow_timestamps)
+
+        tasks_files = list()
+
+        for filename in tasks_filenames:
+            tasks_files.append(sorted(glob(os.path.join(
+                session_directory,
+                tasks_folder,
+                tasks_filename,
+            ))))
+
+        tasks_files = [single for multi in zip(*tasks_files) for single in multi]
+
         timestamps = anylz.roll_through_session(
             session_directory,
             workflow_filename,
             tasks_filename,
             workflow_timestamps,
         )
+
         # TODO aslso assert there is only 1 timestamp for things like start, stop
         assert len(timestamps['workflow']) == 1
         workflowstamps = timestamps['workflow'][0]
@@ -89,4 +121,6 @@ for session_directory in session_directories:
         task_avgdur = np.mean(task_durations)
         task_stddur = np.std(task_durations)
         tasks_durations.append( [nreplicates, task_avgdur, task_stddur] )
+
+print(_data_format_template.format("N Tasks", "W Dur", "T Dur,avg", "T Dur,std", "T Ini,avg", "T Ini,std", "T I,D,avg", "T I,D,std", "T I,I,avg", "T I,I,std", "T Clz,avg", "T Clz,std"))
 
